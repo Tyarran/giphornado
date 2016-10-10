ce3ccb5cac7afa7c98298cb4171a8c523aef694b
Romain Commandë <romain@izberg-marketplace.com>
2016-10-11 00:50:40 +0200

    Display username and command in response

--- giphornado.py
+++ giphornado.py
@@ -9,16 +9,26 @@ from tornado import gen
 
 
 RANDOM_URL = 'http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag={}'
+TEXT = """{user_name} `{command} {tag}`
+{image_url}"""
 
 
 class MainHandler(tornado.web.RequestHandler):
 
     def _get_response(self, response):
         response_payload = json.loads(response.body.decode('utf-8'))
+        tag = ' '.join(self.get_arguments('text')[0].split('+'))
+        user_name = self.get_arguments('user_name')[0]
+        command = self.get_arguments('command')[0]
         image_url = response_payload['data']['image_url']
         result = {
             "response_type": "in_channel",
-            'text': image_url,
+            'text': TEXT.format(
+                user_name=user_name,
+                command=command,
+                tag=tag,
+                image_url=image_url,
+            ),
         }
         return json.dumps(result)
 
